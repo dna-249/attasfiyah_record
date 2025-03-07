976d8a998affb5e000dee927057de260fc47a33f
nuraalhaji <danamonuraa@gmail.com>
2025-03-07 19:49:33 +0100

    commit

--- src/component.jsx/home.jsx
+++ src/component.jsx/home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     const [select,setSelect]= useState()
     const [toggle,setToggle]= useState(false)
 
- const songs =["Ramadan2.mp3","Ramadan3.mp3"]
+ const songs =["Ramadan2.mp3","Ramadan3.mp3","Ramadan4.mp3","Ramadan5.mp3","Ramadan6.mp3","Ramadan7.mp3","Ramadan8.mp3","Ramadan9.mp3","Ramadan10.mp3"]
  
   
  if(toggle === true) {    return (
@@ -14,15 +14,20 @@ const Home = () => {
         <div className="center absolute"><img src="/media2.png" width={200} alt=""/>
         </div>
         {song? 
-        (<div className="blur3" > <h4>PlayList</h4>
-          {songs.map((song,index)=><div key={index} onClick={()=>setSong(false)}>
-        <div className="blur2" onClick={()=>setSelect(song)}>{song}</div></div>)}</div>)
+        (<div className="blur3 l" > <h4>PlayList</h4>
+        <div className="scroll">
+          {songs.map((song,index)=><div  key={index} onClick={()=>setSong(false)}>
+        <div className="blur2" onClick={()=>setSelect(song)}>{song}</div></div>)}</div><div style={{marginTop:'10px'}} className="blur" onClick={()=>setToggle(false)}>BACK TO HOME</div></div>)
         :(<div className="green"> 
           <div className="blur center"><img  src="/mal_salis.jpg" style={{ margin: 'auto',
                                          width:"150px",height:"150px",
                                          borderRadius:"10px"}} alt="" /><br />
         <div>
-         <ReactPlayer url={`/${select}`} className="player" autoPlay controls /></div></div></div>)}
+         <ReactPlayer url={`/${select}`} className="player" autoPlay controls /></div>
+         <div className="grid">
+         <div style={{marginTop:'10px'}} className="blur" onClick={()=>setSong(true)}>BACK</div>
+         <div style={{marginTop:'10px'}} className="blur" onClick={()=>setToggle(false)}> HOME</div>
+         </div></div></div>)}
     </div>
   )} else {
     return (<div className="center auto">
